91e8b5d5bcc8e9a609bf04b26a9be683c4a9543b
lovelish123 <lovelishthapa24@gmail.com>
2022-08-09 12:10:42 +0545

    mycodes

---
+++ c++ codes/ascii_value.cpp
@@ -0,0 +1,14 @@
+//ascii value
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+	char b;
+	cout<<"enter any character";
+	cin>>b;
+	cout<<"ascci value is: "<<int(b)<<"\n";
+ 
+ return 0;
+}
+
